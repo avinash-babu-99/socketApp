15a865eb90603a1b7af13c2a5a1af94033af47b5
avinash-babu-99 <avinashbabu2312@gmail.com>
2023-02-20 20:15:06 +0530

    contacts UI changes

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  @Input() contactsList = [1, 2, 3]
 
   constructor() { }
 
